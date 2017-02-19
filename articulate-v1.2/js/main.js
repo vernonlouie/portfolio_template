140c9d7670f5e45d04558c5db44992452114c01c
Vernon Louie <vernonlouie@aol.com>
2017-02-19 04:11:14 -0800

    almost have the contact form working properly -VL

--- articulate-v1.2/js/main.js
+++ articulate-v1.2/js/main.js
@@ -1,7 +1,46 @@
 $(document).ready(function() {
+    $("#spinner").append("<img src='./mail/ajax-loader.gif'/>");
+    $("#spinner").hide();
 
     /* My Email */
     $("#personal_email").text(" vern864@gmail.com");
+
+    /* Quotes from famous people */
+    var quote_array = [
+        {   quote:  "'Real knowledge is to know the extent of one's ignorance.' ",
+            person: " -Confucius"
+        },
+        {   quote:  "'It does not matter how slowly you go so long as you do not stop.' ",
+            person: " -Confucius"
+        },
+        {   quote:  "'Whenever you find yourself on the side of the majority, it is time to pause and reflect.' ",
+            person: " -Mark Twain"
+        },
+        {   quote:  "'I'm gonna make him an offer he can't refuse.' ",
+            person: " -Vito Corleone"
+        },
+        {   quote:  "Most folks are about as happy as they make up their minds to be.' ",
+            person: " -Lincoln"
+        },
+        {   quote:  "'Don't wait.  The time will never be just right.' ",
+            person: " -Mark Twain"
+        }
+    ];
+
+    var rndm_num = Math.floor(Math.random() * quote_array.length);
+    $("#quote1").text(quote_array[rndm_num].quote);
+    $("#person_quoted1").text(quote_array[rndm_num].person);
+    quote_array.splice(rndm_num, 1);
+
+    rndm_num = Math.floor(Math.random() * quote_array.length);
+    $("#quote2").text(quote_array[rndm_num].quote);
+    $("#person_quoted2").text(quote_array[rndm_num].person);
+    quote_array.splice(rndm_num, 1);
+
+    rndm_num = Math.floor(Math.random() * quote_array.length);
+    $("#quote3").text(quote_array[rndm_num].quote);
+    $("#person_quoted3").text(quote_array[rndm_num].person);
+    quote_array.splice(rndm_num, 1);
 
     /* Scroll hire me button to contact page */
     $('.hire-me').click(function() {
@@ -12,22 +51,20 @@ $(document).ready(function() {
     });
 
     /* For Bootstrap current state on portfolio sorting */
-
     $('ul.nav-pills li a').click(function(e) {
         $('ul.nav-pills li.active').removeClass('active')
         $(this).parent('li').addClass('active')
-    })
+    });
 
     /* portfolio mixitup */
-
     $(window).load(function() {
         var $container = $('.grid-wrapper');
         $container.isotope({
             filter: '*',
             animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
+                duration:   750,
+                easing:     'linear',
+                queue:      false
             }
         });
 
@@ -37,80 +74,73 @@ $(document).ready(function() {
 
             var selector = $(this).attr('data-filter');
             $container.isotope({
-                filter: selector,
-                animationOptions: {
-                    duration: 750,
-                    easing: 'linear',
-                    queue: false
-                }
+                filter:             selector,
+                animationOptions:   {
+                                        duration:   750,
+                                        easing:     'linear',
+                                        queue:      false
+                                    }
             });
             return false;
         });
     });
 
-
     /* Magnific Popup */
     $('.grid-wrapper').magnificPopup({
-        delegate: 'a',
-        type: 'image',
-        gallery: {
-            enabled: true
-        }
+        delegate:   'a',
+        type:       'image',
+        gallery:    {
+                        enabled: true
+                    }
     });
-
 
     /* Sticky menu */
     $(".navbar").sticky({
         topSpacing: 0
     });
 
-
     /* Scroll spy and scroll filter */
     $('#main-menu').onePageNav({
-        currentClass: "active",
-        changeHash: false,
-        scrollThreshold: 0.5,
-        scrollSpeed: 750,
-        filter: "",
-        easing: "swing"
+        currentClass:       "active",
+        changeHash:         false,
+        scrollThreshold:    0.5,
+        scrollSpeed:        750,
+        filter:             "",
+        easing:             "swing"
     });
 
     /* Charts*/
-
     $('.chart').waypoint(function() {
         $(this).easyPieChart({
-            barColor: '#3498db',
-            size: '150',
-            easing: 'easeOutBounce',
-            onStep: function(from, to, percent) {
-                $(this.el).find('.percent').text(Math.round(percent));
-            }
+            barColor:   '#3498db',
+            size:       '150',
+            easing:     'easeOutBounce',
+            onStep:     function(from, to, percent) {
+                            $(this.el).find('.percent').text(Math.round(percent));
+                        }
         });
     }, {
         triggerOnce: true,
         offset: 'bottom-in-view'
     });
 
-
     /* VEGAS Home Slider */
-	
-	    $('#page-welcome').vegas({
-        slides: [
-            { src: 'img/slider/IMG_9096.jpg' },
-            { src: 'img/slider/20161030_120811.jpg' },
-            { src: 'img/slider/20161029_144458.jpg' },
-			{ src: 'img/slider/IMG_9027.jpg' }
-        ],
+    $('#page-welcome').vegas({
+    slides: [
+                { src: 'img/slider/IMG_9096.jpg' },
+                { src: 'img/slider/20161030_120811.jpg' },
+                { src: 'img/slider/20161029_144458.jpg' },
+                { src: 'img/slider/IMG_9027.jpg' }
+            ],
     overlay: true
-});
+    });
 
     $("#vegas-next").click(function() {
         $('#page-welcome').vegas('next');
     });
+
     $("#vegas-prev").click(function() {
        $('#page-welcome').vegas('previous');
     });
-
-
 
 });
--- articulate-v1.2/mail/contact_me.php
+++ articulate-v1.2/mail/contact_me.php
@@ -1,27 +1,100 @@
 <?php
 // check if fields passed are empty
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-	echo "No arguments Provided!";
-	return false;
-   }
-	
-$name = $_POST['name'];
-$email_address = $_POST['email'];
-$message = $_POST['message'];
-	
-// create email body and send it	
-$to = 'you@yourdomain.com'; // put your email address here
-$email_subject = "Contact form submitted by:  $name";
-$email_body = "You have received a new message. \n\n".
-				  " Here are the details:\n \nName: $name \n ".
-				  "Email: $email_address\n Message: \n $message";
-$headers = "From: noreply@yourdomain.com\n"; 
-// Since this email form will be generated from your server. The From email address will be best using something like this noreply@yourdomain.com
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
-return true;			
+//if(empty($_POST['name'])  		||
+//   empty($_POST['email']) 		||
+//   empty($_POST['message'])	||
+//   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+//   {
+//	echo "No arguments Provided!";
+//	return false;
+//   }
+//
+//$name = addslashes( $_POST['name'] );
+//$email_address = addslashes( $_POST['email'] );
+//$message = addslashes( $_POST['message'] );
+//
+//// create email body and send it
+//$to = 'you@yourdomain.com'; // put your email address here
+//$email_subject = "Contact form submitted by:  $name";
+//$email_body = "You have received a new message. \n\n".
+//				  " Here are the details:\n \nName: $name \n ".
+//				  "Email: $email_address\n Message: \n $message";
+//$headers = "From: noreply@yourdomain.com\n";
+//// Since this email form will be generated from your server. The From email address will be best using something like this noreply@yourdomain.com
+//$headers .= "Reply-To: $email_address";
+//mail($to,$email_subject,$email_body,$headers);
+//return true;
+
+require_once('email_config.php');
+require('phpmailer/PHPMailer/PHPMailerAutoload.php');
+
+$mail = new PHPMailer;
+$mail-> SMTPDebug = 0;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+
+$mail->Username = EMAIL_USER;                 // SMTP username
+$mail->Password = EMAIL_PASS;                 // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                                    // TCP port to connect to
+$options = array(
+    'ssl' => array(
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => true
+    )
+);
+
+$email = addslashes($_POST["email"]);
+$subject = addslashes( $_POST["subject"]);
+$body = addslashes( $_POST["message"] );
+
+$mail->smtpConnect($options);
+$mail->From = 'nrev864@gmail.com';//your email sending account
+$mail->FromName = 'nrev';//your email sending account name
+$mail->addAddress("vern864@gmail.com", "Vern");     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+$mail->addReplyTo($email); /* (email address of the person sending the message, so you can reply) */
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = $subject;
+$mail->Body    = $body;
+$mail->AltBody = html_entity_decode($body);
+
+$output = [];
+
+$name = addslashes($_POST["name"]);
+if ($name === "") {
+    $output["errors"][] = "There is no name.";
+}
+
+if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+    $nameErr = "Name - Only letters and white space allowed";
+    $output["errors"][] = $nameErr;
+}
+
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $emailErr = "Email - Invalid format";
+    $output["errors"][] = $emailErr;
+}
+
+if ($subject === "") {
+    $output["errors"][] = "There is no subject.";
+}
+
+if(!$mail->send()) {
+    $output["errors"][] = 'Message could not be sent.';
+    $output["errors"][] = "Mailer Error: " . $mail->ErrorInfo;
+} else {
+    $output["success"][] = 'Message has been sent';
+}
+
+echo json_encode($output);
 ?>
